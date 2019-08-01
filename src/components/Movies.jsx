caa5b269b4182a0484bb79566735a55178283e63
Steven Lim <limstevenlbw@gmail.com>
2019-07-31 23:44:05 -0700

    Filter Options Completed

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,14 +12,16 @@ class Movies extends Component {
         movies: [],
         pageSize: 4,
         currentPage: 1,
-        genres: []
+        genres: [],
+        selectedGenre: null
     }
 
     //When an instance of this component is rendered in the dom, trigger
     componentDidMount() {
         //Movies and Genres were initialized to empty arrays at first to ensure they were not undefined, once a server response
-        //is acquired, we update the component state
-        this.setState({ movies: getMovies(), genres: getGenres() })
+        //is acquired, we update the component state, include an All Genres Filter
+        const genres = [{ name: 'All Genres' }, ...getGenres()];
+        this.setState({ movies: getMovies(), genres })
     }
     /*
         OnClick handler to delete movies from the displayed table
@@ -47,17 +49,23 @@ class Movies extends Component {
     };
 
     handleGenreSelect = (genre) => {
-        console.log(genre);
+        //Also reset current page to 1 to avoid bugs
+        this.setState({ selectedGenre: genre, currentPage: 1 })
     }
 
     render() {
         //object destructuring
-        const { length: count } = this.state.movies;
-        const { pageSize, currentPage, movies: allMovies } = this.state;
+        //const { length: count } = this.state.movies;
+        const { pageSize, currentPage, movies: allMovies, selectedGenre } = this.state;
 
-        const movies = paginate(allMovies, currentPage, pageSize);
+        //If there is a selected genre(All Genres doesnt have an id), implement a filter
+        const filteredMovies = selectedGenre && selectedGenre._id ?
+            allMovies.filter(movie => movie.genre._id === selectedGenre._id)
+            : allMovies
 
-        if (count === 0) {
+        const movies = paginate(filteredMovies, currentPage, pageSize);
+
+        if (filteredMovies.length === 0) {
             return <p>
                 Looks like there aren't any movies in the database
             </p>
@@ -65,10 +73,16 @@ class Movies extends Component {
         //2 Column Bootstrap Layout
         return (
             <div className="row">
-                <div className="col-2">
-                    <ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect} />
+                <div className="col-3">
+                    <ListGroup
+                        items={this.state.genres}
+                        onItemSelect={this.handleGenreSelect}
+                        textProperty="name"
+                        valueProperty="_id"
+                        selectedItem={this.state.selectedGenre}
+                    />
                 </div>
-                <div className="col"> <p>Showing {count} movies in the database</p>
+                <div className="col"> <p>Showing {filteredMovies.length} movies in the database</p>
                     <table className="table">
                         <thead>
                             <tr>
@@ -101,7 +115,7 @@ class Movies extends Component {
                     </table>
 
                     <Pagination
-                        itemsCount={count}
+                        itemsCount={filteredMovies.length}
                         pageSize={pageSize}
                         onPageChange={this.handlePageChange}
                         currentPage={currentPage}
--- src/components/common/ListGroup.jsx
+++ src/components/common/ListGroup.jsx
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
+//BootStrap ListGroup component
+const ListGroup = (props) => {
+    //Decoupled to use any json list
+    const { items, textProperty, valueProperty, onItemSelect, selectedItem } = props;
 
-const ListGroup = () => {
-    return ( null );
+    return (
+        < ul className="list-group">
+            {items.map(item => (
+                <li key={item[valueProperty]}
+                    className={item === selectedItem ? "list-group-item active" : "list-group-item"}
+                    onClick={() => onItemSelect(item)} >
+                    {item[textProperty]}
+                </li>
+            ))
+            }
+        </ul >);
 }
- 
+
+ListGroup.defaultProps = {
+    textProperty: "name",
+    valueProperty: "_id"
+};
+
 export default ListGroup;
